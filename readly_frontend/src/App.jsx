66ddb9630d2f24070827ae8de46867c646fff193
Auyeol <rmaduf3016@naver.com>
2024-07-22 10:40:59 +0900

    init frontend

---
+++ readly_frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="font-BM_HANNA font-bold">안녕하세요</h1>
+      <h1 className="font-bold">안녕하세요</h1>
+    </>
+  )
+}
+
+export default App
