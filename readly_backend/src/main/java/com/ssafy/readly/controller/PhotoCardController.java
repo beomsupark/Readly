64135c2d738f061f9c4c88aa63400f857b2d13f2
Auyeol <rmaduf3016@naver.com>
2024-07-30 08:34:12 +0900

    Feat: 메인페이지 API

--- readly_backend/src/main/java/com/ssafy/readly/controller/PhotoCardController.java
+++ readly_backend/src/main/java/com/ssafy/readly/controller/PhotoCardController.java
@@ -17,6 +17,7 @@ import com.ssafy.readly.service.photocard.PhotoCardServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +30,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PhotoCardController {
 
     private final PhotoCardService PhotoCardServiceImpl;
+    @Autowired
     private final AIService aiService;
     private final BookService bookService;
     private final MemberService memberService;
--- readly_frontend/src/api/bookAPI.js
+++ readly_frontend/src/api/bookAPI.js
@@ -5,6 +5,7 @@ const BASE_URL = 'http://localhost:8080'; // Spring Boot 서버 주소
 export const fetchBooks = async () => {
   try {
     const response = await axios.post(`${BASE_URL}/book/searchBooks`);
+    console.log(response)
     return response.data.books;
   } catch (error) {
     console.error('Error fetching books:', error);
