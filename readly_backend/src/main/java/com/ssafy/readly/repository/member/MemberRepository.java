41a3d2b4581ebf83b311bc7d33773ef7a52b4116
KimShinBeom <gotwo6141922@naver.com>
2024-07-30 18:09:10 +0900

    feat : 회원정보 기능 추가

--- readly_backend/src/main/java/com/ssafy/readly/repository/member/MemberRepository.java
+++ readly_backend/src/main/java/com/ssafy/readly/repository/member/MemberRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.readly.repository.member;
 import com.ssafy.readly.dto.member.FindMemberRequest;
 import com.ssafy.readly.dto.member.LoginMemberRequest;
 import com.ssafy.readly.dto.member.LoginMemberResponse;
+import com.ssafy.readly.dto.member.UpdateMemberRequest;
 import com.ssafy.readly.entity.Member;
 
 import java.util.Optional;
@@ -15,7 +16,11 @@ public interface MemberRepository {
 
     public abstract void logout(String userId);
 
-    public abstract Optional<Member> findByLoginId(String loginId);
+    public abstract Optional<Member> findById(int id);
+
+    public abstract Long findByLoginId(String loginId);
+
+    public abstract void updateMember(UpdateMemberRequest UpdateMember);
 
     public abstract String checkMember(FindMemberRequest findMember);
 
