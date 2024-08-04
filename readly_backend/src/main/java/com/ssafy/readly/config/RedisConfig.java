70e2eb3d3cbada1c55f988b8c1bb878704e2ca58
KimShinBeom <gotwo6141922@naver.com>
2024-08-04 21:36:06 +0900

    revert : RedisConfig 되돌려지지 않은 부분

--- readly_backend/src/main/java/com/ssafy/readly/config/RedisConfig.java
+++ readly_backend/src/main/java/com/ssafy/readly/config/RedisConfig.java
@@ -35,7 +35,7 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
         return container;
