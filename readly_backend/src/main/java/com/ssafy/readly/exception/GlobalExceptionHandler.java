638c4461796c46a7d33bcdebf8b11fbb754aa2cb
KimShinBeom <gotwo6141922@naver.com>
2024-08-07 11:29:52 +0900

    feat : 로그인, 회원가입 검증 추가, 에러메세지 수정
    에러 메시지 수정(loginId를 입력해주세요. -> 아이디를 입력해주세요.)
    회원 가입 시 비밀번호 길이, 특수문자 포함 여부 체크 추가

--- readly_backend/src/main/java/com/ssafy/readly/exception/GlobalExceptionHandler.java
+++ readly_backend/src/main/java/com/ssafy/readly/exception/GlobalExceptionHandler.java
@@ -5,8 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.security.sasl.AuthenticationException;
 import java.util.HashMap;
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class  GlobalExceptionHandler {
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
@@ -22,17 +22,26 @@ public class  GlobalExceptionHandler {
         final List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
         Map<String,Object> map = new HashMap<>();
         StringBuilder message = new StringBuilder();
+        String longMsg = "";
 
         for (FieldError fieldError : fieldErrors) {
-            String field = fieldError.getField();
+            String field = fieldError.getDefaultMessage();
+            if(field.length() > 6) {
+                longMsg += " " + field;
+                continue;
+            }
             message.append(field).append(", ");
         }
 
-        if (!fieldErrors.isEmpty()) {
+        if (!message.isEmpty()) {
             message.setLength(message.length() - 2); // 마지막 ", " 제거
+            message.append("의 값을 입력해주세요.");
         }
 
-        message.append("의 값을 입력해주세요.");
+        if(!longMsg.isEmpty()) {
+            message.append(longMsg);
+        }
+
         map.put("errorMessage", message);
         return new ResponseEntity<Map<String,Object>>(map, HttpStatus.BAD_REQUEST);
     }
