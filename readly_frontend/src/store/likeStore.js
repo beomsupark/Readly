5eaec2e96e30ed4df1dbda48da87e0a0fb421e72
koo821 <dhsdbtksemf@naver.com>
2024-08-12 15:27:49 +0900

    Fix : 좋아요 수정

--- readly_frontend/src/store/likeStore.js
+++ readly_frontend/src/store/likeStore.js
@@ -1,53 +1,37 @@
-// likeStore.js
-import create from 'zustand';
-import { persist } from 'zustand/middleware';
+import { create } from 'zustand';
 import { addLike, removeLike } from '../api/likeAPI';
 
-const useLikeStore = create(persist(
-  (set, get) => ({
-    likes: {},
-    toggleLike: async (userId, itemId, itemType) => {
-      const currentLikes = get().likes;
-      const isLiked = currentLikes[itemId];
-
-      // 먼저 상태를 변경하여 사용자에게 즉각적인 피드백을 제공
-      set(state => ({
-        likes: {
-          ...state.likes,
-          [itemId]: !isLiked
-        }
-      }));
-
-      try {
-        if (isLiked) {
-          await removeLike(userId, itemType === 'review' ? itemId : null, itemType === 'photoCard' ? itemId : null);
-        } else {
-          await addLike(userId, itemType === 'review' ? itemId : null, itemType === 'photoCard' ? itemId : null);
-        }
-      } catch (error) {
-        // 에러가 발생하면 좋아요 상태를 다시 원래대로 되돌림
-        set(state => ({
-          likes: {
-            ...state.likes,
-            [itemId]: isLiked
-          }
-        }));
-        console.error('Error toggling like:', error);
-      }
-    },
-    setInitialLikeStatus: (itemId, status) => {
-      set(state => ({
-        likes: {
-          ...state.likes,
-          [itemId]: status
-        }
-      }));
+const useLikeStore = create((set, get) => ({
+  likes: JSON.parse(localStorage.getItem('likes')) || {},
+  
+  toggleLike: async (memberId, itemId, isReview = false) => {
+    const currentLikes = get().likes;
+    const isLiked = currentLikes[itemId];
+    
+    if (isLiked) {
+      // 좋아요 취소
+      await removeLike(memberId, isReview ? itemId : null, isReview ? null : itemId);
+      const updatedLikes = { ...currentLikes, [itemId]: false };
+      localStorage.setItem('likes', JSON.stringify(updatedLikes));
+      set({ likes: updatedLikes });
+      return false;
+    } else {
+      // 좋아요 추가
+      await addLike(memberId, isReview ? itemId : null, isReview ? null : itemId);
+      const updatedLikes = { ...currentLikes, [itemId]: true };
+      localStorage.setItem('likes', JSON.stringify(updatedLikes));
+      set({ likes: updatedLikes });
+      return true;
     }
-  }),
-  {
-    name: 'like-storage',
-    getStorage: () => localStorage,
-  }
-));
+  },
+
+  setInitialLikeStatus: (itemId, status) => {
+    set((state) => {
+      const updatedLikes = { ...state.likes, [itemId]: status };
+      localStorage.setItem('likes', JSON.stringify(updatedLikes));
+      return { likes: updatedLikes };
+    });
+  },
+}));
 
 export default useLikeStore;
